1eae4ca3149ff5fde2312462529e55448b06bf62
Raman1121 <happy07.dutt@gmail.com>
2022-02-08 01:04:48 +0000

    argparse removed for config file

--- src/train_DR_model.py
+++ src/train_DR_model.py
@@ -10,22 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 
-
-import argparse
 import yaml
-
-# parser = argparse.ArgumentParser(description='Hyper-parameters management')
-
-# parser.add_argument('--batch_size', type=int, default=4, help='Batch Size')
-# parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
-# parser.add_argument('--encoder', type=str, default='resnet18', help='Encoder model')
-# parser.add_argument('--num_classes', type=int, default=5, help='Number of target classes')
-# parser.add_argument('--lr', type=float, default=1e-3, help='Learning Rate')
-# parser.add_argument('--gpus', type=int, default=0, help='Number of GPUs')
-# parser.add_argument('--train_df_path', type=str, default='/home/raman/MLP-RIQA/Train_set_RIQA_DR_Labels.csv', help='Path to read the training dataset file')
-# parser.add_argument('--cat_labels', type=list, default=['Good', 'Usable', 'Reject'], help='Categorical Quality labels to include in the training set')
-
-# args = parser.parse_args()
 
 with open('config_train_DR.yaml') as file:
     yaml_data = yaml.safe_load(file)
@@ -49,5 +34,5 @@ classifier = retinopathy_model.RetinopathyClassificationModel(encoder=yaml_data[
                                                             num_classes=yaml_data['train']['num_classes'], lr=yaml_data['train']['lr'])
 
 trainer = pl.Trainer(progress_bar_refresh_rate=20, gpus=yaml_data['train']['gpus'], max_epochs=yaml_data['train']['epochs'])  
-trainer.fit(classifier, train_loader)  # train_loader
+trainer.fit(classifier, train_loader)  
 
